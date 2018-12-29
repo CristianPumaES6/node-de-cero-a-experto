fd1d370bc1fed7149b07b4bbe37ddc0f5274d19b
Cristian Angel Puma Villalva <cristian.puma@gramd.com>
2018-12-29 18:57:39 -0500

    Importar archivo, require (multiplicar)

--- 2_bases node/1_tablas/multiplicar.js
+++ 2_bases node/1_tablas/multiplicar.js
@@ -4,8 +4,13 @@ crearArchivo = (base) => {
     return new Promise((resolve, reject) => {
         let data = '';
 
+        if(!Number(base)){
+            reject(`error, La base ingresada no es un numero.`);
+            return;
+        }
+
         for (let i = 1; i <= 10; i++) {
-            data += `${base} * ${i} = ${base * i}`;
+            data += `${base} * ${i} = ${base * i} \n`;
         }
 
         fs.writeFile(`1_tabla-${base}.txt`, data, (err) => {
